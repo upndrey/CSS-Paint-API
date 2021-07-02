1245d2682e0c09818ce200695a9faf6931b72fcb
Andrey <upndrey@yandex.ru>
2021-07-02 18:33:53 +0700

    test

--- index.js
+++ index.js
@@ -1,20 +1,5 @@
-registerPaint('headerHighlight', class {
-
-  /*
-       define if alphatransparency  is allowed alpha
-       is set to true by default. If set to false, all
-       colors used on the canvas will be fully opaque
-    */
-  static get contextOptions() {
-           return { alpha: true };
-    }
-
-    /*
-        ctx is the 2D drawing context
-        a subset of the HTML5 Canvas API.
-    */
-  paint(ctx) {
-        ctx.fillStyle = 'hsla(25, 90%, 60%, 1.0)';
-        ctx.fillRect(0, 15, 200, 20);     /* order: x, y, w, h */
+registerPaint('box', class {
+  paint(ctx, geom, properties) {
+    ctx.fillRect(0, 0, geom.width, geom.height)
   }
-});
+})
